ee8940f98555d848bf2a10617d9d0749eb26eaac
JP <joao.virgili@carupi.com>
2021-01-06 22:02:08 -0300

    fix: imports

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,8 @@
 import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
-import 'package:cloudwalk_nasa/app/app_widget.dart';
-import 'package:cloudwalk_nasa/app/modules/home/home_module.dart';
+import 'app_widget.dart';
+import 'modules/home/home_module.dart';
 
 class AppModule extends MainModule {
   @override
