ebfd3c2a4bf41e811f21440505ae5334cafe1b5f
JP <joao.virgili@carupi.com>
2021-01-08 22:02:55 -0300

    feat: finish apod details page

--- lib/app/modules/home/pages/apod_details/apod_details_page.dart
+++ lib/app/modules/home/pages/apod_details/apod_details_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../../domain/entities/apod_entity.dart';
+import '../../../../shared/formatters/date_formatter.dart';
 import 'apod_details_controller.dart';
 
 class ApodDetailsPage extends StatefulWidget {
@@ -20,8 +21,32 @@ class _ApodDetailsPageState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: <Widget>[],
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: <Widget>[
+          const SizedBox(height: 10),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Image.network(
+              widget.apodEntity.mediaType == "video" &&
+                      widget.apodEntity.thumbnailUrl != ""
+                  ? widget.apodEntity.thumbnailUrl
+                  : widget.apodEntity.url,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            widget.apodEntity.title,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            DateFormatter.format(widget.apodEntity.date),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          Text(widget.apodEntity.explanation),
+        ],
       ),
     );
   }
